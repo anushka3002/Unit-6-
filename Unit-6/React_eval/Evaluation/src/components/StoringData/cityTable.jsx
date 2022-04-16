f3bf984584dbeaa20240955864e4ba66559e6fd8
Anushka <95957335+anushka3002@users.noreply.github.com>
2022-04-17 00:56:14 +0530

    init

--- Unit-6/React_eval/Evaluation/src/components/StoringData/cityTable.jsx
+++ Unit-6/React_eval/Evaluation/src/components/StoringData/cityTable.jsx
@@ -2,21 +2,19 @@ import { Link } from "react-router-dom";
 
 export const CityTable = ({ deleteCity,data }) => {
   return (
+    <table>
     <tr>
       <td>{data.id}</td>
       <td>{data.country}</td>
       <td>{data.city}</td>
       <td>{data.population}</td>
       <td>
-        <button>
-          <Link to={`/update/${data.id}`}>Edit it</Link>
-        </button>
+      <Link to={`/update/${data.id}`}><button>Edit it</button></Link>
       </td>
       <td>
-        <button onClick={()=>deleteCity(data.id)}>
-          Delete
-        </button>
+        <button onClick={()=>deleteCity(data.id)}>Delete</button>
       </td>
     </tr>
+    </table>
   );
 };
