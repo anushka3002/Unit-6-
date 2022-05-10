8d33fe4e3f8ea697694fbc543f07e44d87cd52a4
Anushka <95957335+anushka3002@users.noreply.github.com>
2022-05-10 10:54:11 +0530

    made the homepage, login, register UI

---
+++ login_signup/src/Components/Homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react" 
+import "./homepage.css"
+
+export const Homepage=()=>{
+    return(
+        <>
+        <div className="homepage">
+            <h1>Hello homepage</h1>
+            <div className="button">Logout</div>
+        </div>
+        </>
+    )
+}
